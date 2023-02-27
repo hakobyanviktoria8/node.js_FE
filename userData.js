66a909f35c63623af0e372a65f8dd08bc3301087
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-02-27 14:51:42 +0400

    in FE part send req, take all user data and render inside UI

--- userData.js
+++ userData.js
@@ -1,15 +1,24 @@
 window.addEventListener("load", () => {
-    document.querySelector("#userDataBtn").addEventListener("click", () => {
-        fetch("http://localhost:3000/users") //?name=8642
-            .then(res => res.json())
-            .then(userData=> console.log(userData.data))
+    document.querySelector("#userDataBtn").addEventListener("click", async () => {
+        const res = await fetch('http://localhost:3000/users');
+        const userData = await res.json();
+        console.log(userData.data)
+        // fetch("http://localhost:3000/users") //?name=8642
+        //     .then(res => res.json())
+        //     .then(userData=> console.log(userData.data))
+        const userDataArr = userData.data
+        
+        document.querySelector("#userData").innerHTML += 
+            `${userDataArr.map(user =>
+                `<li><a href="#">${user.username}</a></li>`
+                ).join('')}`
     })
 
     document.querySelector("#addUserBtn").addEventListener("click", async () => {
         const username = document.querySelector('input[name="username"]').value
         const name = document.querySelector('input[name="name"]').value
         const image = document.querySelector('input[name="image"]').files[0]
-        console.log("Done!", username, name, image);
+        console.log("Done0000!", username, name, image);
         
         // create form data
         const form = new FormData();
@@ -27,8 +36,8 @@ window.addEventListener("load", () => {
             },
             body: form,
         });
-        const userData = await res.json();
-        console.log(userData)       
+        const userData = await res.json();        
+        console.log("Done2222!",userData)       
     })
 
 })
